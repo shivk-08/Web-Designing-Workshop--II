c18632355fbc076e4969748a2a80e57edc5cab03
Shiv Kumar <shivkumar08102006@gmail.com>
2026-05-12 21:53:19 +0530

    new code

---
+++ Exp9/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    alert("Registration Successful");
+
+    console.log({
+      name,
+      email,
+      password,
+    });
+  };
+
+  return (
+    <div className="container">
+      <form className="form-box" onSubmit={handleSubmit}>
+        <h1>Registration Form</h1>
+
+        <input
+          type="text"
+          placeholder="Enter Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
